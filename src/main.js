13cfcd38f91717c74f923df38225ff264ce27a64
Дмитрий Петров <d.petrov@jetmail.cc>
2020-04-08 23:22:32 +0300

    vue-cookies package added and default cookie expire time set to 30 days window.vm variable created

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from "vue";
+import VueCookies from "vue-cookies";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
@@ -6,7 +7,10 @@ import "./registerServiceWorker";
 
 Vue.config.productionTip = false;
 
-new Vue({
+Vue.use(VueCookies);
+Vue.$cookies.config("30d");
+
+window.vm = new Vue({
   router,
   store,
   render: h => h(App)

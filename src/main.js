897478091aa1e1ae0e4a137ad34bd90487309154
Дмитрий Петров <d.petrov@jetmail.cc>
2020-04-09 02:01:25 +0300

    Movie vue-cookies config into separated file

--- src/main.js
+++ src/main.js
@@ -1,16 +1,12 @@
 import Vue from "vue";
-import VueCookies from "vue-cookies";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "./cookies";
 import "./debugLevel";
 import "./registerServiceWorker";
 
 Vue.config.productionTip = false;
-
-Vue.use(VueCookies);
-Vue.$cookies.config("30d");
-
 window.vm = new Vue({
   router,
   store,

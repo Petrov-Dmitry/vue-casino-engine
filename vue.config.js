68369bdfaec4993bc51aa1f1f548e88fdbc1bdc1
Дмитрий Петров <d.petrov@jetmail.cc>
2020-04-08 22:17:09 +0300

    VUE application created and configured by vue ui

---
+++ vue.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  pwa: {
+    workboxPluginMode: "InjectManifest",
+    name: "vue-casino-engine",
+    themeColor: "#4c4c4c",
+    msTileColor: "#1c1e1a",
+    manifestCrossorigin: "use-credentials",
+    manifestOptions: {
+      background_color: "#1c1e1a"
+    }
+  },
+
+  assetsDir: "assets",
+  runtimeCompiler: true,
+
+  css: {
+    sourceMap: true
+  }
+};
